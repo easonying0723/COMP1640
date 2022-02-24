0ef82423734803cce10084fa810049b91daf4916
easonying0723 <ey73210723@gmail.com>
2022-02-24 12:03:39 +0800

    ccommit

--- discussion/resources/views/testyyy.php
+++ discussion/resources/views/testyyy.php
@@ -341,27 +341,4 @@
       // Your code to run since DOM is loaded and ready
    });
 
-   var t = $('#stockadjtable').DataTable({//call table id
-
-      //retrieve: true,
-      //scrollCollapse: true,
-      scroll: true,
-      responsive: true,
-      searching: true,
-      paging: true,
-
-      columnDefs: [ {
-         targets: 6,
-         data: null,
-         defaultContent:'<input type="checkbox" id="userstatus" data-toggle="toggle" data-on="On" data-off="Off" data-size="sm" checked required>'
-         },
-
-      {
-         targets: -1,
-         data: null,
-         defaultContent: '<button class="btn btn-light" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="bx bx-trash"></i></button>'
-      }],
-
-   });
-
 </script>
