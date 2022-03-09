8b6a1d1d2c722d3b2dfed0519c9811d3fbbd8777
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-03-09 09:13:14 +0800

    Update homepage.blade.php

--- discussion/resources/views/homepage.blade.php
+++ discussion/resources/views/homepage.blade.php
@@ -287,7 +287,7 @@
       <div class="container-fluid">
                     <div class="row">
                        <div class="col-md-6">
-                          <label for="">Category:</label>
+                          <label for="">Category Name:</label>
                           <input type="text" class="form-control" id="" placeholder="Please fill in..." required>
                        </div>
                        <div class="col-md-6">
@@ -314,4 +314,21 @@
 </div>
                     
             <!---------------------------------------End Of Modal---------------------------------------------->
+            <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <div class="modal-header">
+               <h5 class="modal-title" id="exampleModalLabel">Succesfully Added.</h5>
+               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+               </div>
+               <div class="modal-body">
+                  <label>New Category Added.</label>
+                  
+               </div>
+               <div class="modal-footer">
+               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Notice</button>
+               </div>
+            </div>
+         </div>
+      </div>
 @endsection
