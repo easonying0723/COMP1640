e05845919c72bc85f4eb3eeb714b993916725b79
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-06 14:19:42 +0800

    Update homepage.blade.php

--- discussion/resources/views/homepage.blade.php
+++ discussion/resources/views/homepage.blade.php
@@ -37,86 +37,31 @@
          </div>
          <div class="accordion accordion-flush overflow-auto p-2 bg-transparent" style="max-height: 630px;"
             id="accordionFlushExample">
+            @foreach($data as $categoryData)
             <div class="accordion-item flex">
+
                <h5 class="accordion-header" id="flush-headingOne">
                   <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                      data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                     #FACILITIES
+                     #{{$categoryData->cate_name}}
+                           <button>
+                              <a href="/delete/{{$categoryData->id}}" class="la la-times">Delete</a>
+                           </button>
+                     
                      <span class="position-absolute top-10 start-100 translate-middle p-1 bg-warning rounded-circle">
                         <span class="visually-hidden">New alerts</span>
                      </span>
                   </button>
-               </h5>
-               <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
-                  data-bs-parent="#accordionFlushExample">
-                  <div class="accordion-body">
-                     <div class="list-group" id="categoryListGroup">
-                        <a href="#"
-                           class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                           #CBEASON
-                           <span class="badge badge-primary badge-pill">
-                              11
-                           </span>
-                        </a>
-                        <a href="#"
-                           class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                           #TOILET
-                           <span class="badge badge-primary badge-pill">
-                              2
-                           </span>
-                        </a>
-                        <a href="#"
+               </h5>---->
+               <!--------------LIST GROUP INSIDE---------
+                <a href="#"
                            class="list-group-item list-group-item-dark list-group-item-action d-flex justify-content-between align-items-center"
                            id="addtitle">
                            + ADD TITLE
                         </a>
                      </div>
-                  </div>
-               </div>
-            </div>
-            <div class="accordion-item">
-               <h2 class="accordion-header" id="flush-headingTwo">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                     data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                     #LECTURER
-                     <span class="position-absolute top-10 start-100 translate-middle p-1 bg-warning rounded-circle">
-                        <span class="visually-hidden">New alerts</span>
-                     </span>
-                  </button>
-               </h2>
-               <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
-                  data-bs-parent="#accordionFlushExample">
-                  <div class="accordion-body">
-                     <a href="#"
-                        class="list-group-item list-group-item-dark list-group-item-action d-flex justify-content-between align-items-center"
-                        id="addtitle">
-                        + ADD TITLE
-                     </a>
-                  </div>
-               </div>
-            </div>
-            <div class="accordion-item">
-               <h2 class="accordion-header" id="flush-headingThree">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                     data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                     #NOTHING
-                     <span class="position-absolute top-10 start-100 translate-middle p-1 bg-warning rounded-circle">
-                        <span class="visually-hidden">New alerts</span>
-                     </span>
-                  </button>
-               </h2>
-               <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree"
-                  data-bs-parent="#accordionFlushExample">
-                  <div class="accordion-body">
-                     <a href="#"
-                        class="list-group-item list-group-item-dark list-group-item-action d-flex justify-content-between align-items-center"
-                        id="addtitle">
-                        + ADD TITLE
-                     </a>
-                  </div>
-               </div>
-            </div>
-         </div>
+                  @endforeach
+               </div>--->
       </div>
    </div>
    <div class="col-md-9 content" style="background-color: #A7B7CD">
