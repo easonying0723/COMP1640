7c1865b98b5a5a925eb1c981c179cb111e688a63
easonying0723 <ey73210723@gmail.com>
2022-04-08 09:17:41 +0800

    update

--- discussion/resources/views/homepage.blade.php
+++ discussion/resources/views/homepage.blade.php
@@ -381,13 +381,7 @@
                   <small style="float: right; margin: 10px">78 Comments</small>
                </div>
             </div>
-<<<<<<< Updated upstream
             <hr />
-=======
-            <hr/>
-
-            
->>>>>>> Stashed changes
             <div class="container">
                <div class="modal-body" id="footersec">
                   <form action="{{ route('homepage.store_comment', ['id' => $idea->id ])}}" method="post">
