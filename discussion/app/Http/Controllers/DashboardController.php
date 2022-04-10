4bda3d261d98b070c4f9766a3ccaccea76dc0e13
callmecam26 <100278322+callmecam26@users.noreply.github.com>
2022-04-11 00:16:57 +0800

    homepage and dashboard update

--- discussion/app/Http/Controllers/DashboardController.php
+++ discussion/app/Http/Controllers/DashboardController.php
@@ -1,7 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Idea;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
 class DashboardController extends Controller
 {
     /**
@@ -21,22 +26,47 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $departments = array(array("id"=>1,"name"=>'Department 1'),array("id"=>2,"name"=>'Department 2'));
-        $titles = array(array("id"=>1,"name"=>'Title 1'),array("id"=>2,"name"=>'Title 2'),array("id"=>3,"name"=>'Title 3'),array("id"=>4,"name"=>'Title 4'),array("id"=>5,"name"=>'Title 5'));
-        $ideasWithoutComment = array(array("ID"=>1,"idea"=>'Idea Post'),array("ID"=>2,"idea"=>'Test'));
-        $anonymous = array(array("id"=>1,"idea"=>'Idea Post 1',"comment"=>'Comment 1',"thumbsup"=>'1',"thumbsdown"=>'1'),array("id"=>1,"idea"=>'Idea Post 2',"comment"=>'Comment 2',"thumbsup"=>'2',"thumbsdown"=>'2'));
 
-        $chart1XValues = array("January", "February", "March", "April", "May","June","July");
-        $chart1yValues = array(65, 58, 80, 83, 55,53,40);
-        $chart1Colors = array("red", "green","blue","orange","brown","purple","yellow");
+        $departmentsArray = DB::table('users')->select('department')->distinct()->get()->toArray();
+        $departments = array();
+        foreach ($departmentsArray as $key => $value) {
+            $departments[$key] = array("id"=>intval($key)+1,"name"=>$value->department);
+        }
+        
+        $contribution_of_title = Idea::get()->count();
+
+        $titles = array(array("id"=>1,"name"=>'Title 1'),array("id"=>2,"name"=>'Title 2'),array("id"=>3,"name"=>'Title 3'),array("id"=>4,"name"=>'Title 4'),array("id"=>5,"name"=>'Title 5'));
+
+        $anonymousIdea = Idea::where('anonymous',1)->paginate(5,['*'], 'anonymous_idea');
+        $anonymousComment = Comment::where('anonymity',1)->paginate(5, ['*'], 'anonymous_comment');
+
+        $chart1Data = DB::table('idea')
+        ->leftJoin('users', 'users.id', '=', 'idea.user_id')
+        ->select(DB::raw('department,count(idea.id) as number_of_idea'))
+        ->groupBy('users.department')
+        ->get()
+        ->toArray();
+        $chart1XValues = array_column($chart1Data,'department');
+        $chart1yValues = array_column($chart1Data,'number_of_idea');
+        $chart1Colors = array("red", "green","blue","orange","brown","purple","yellow","black","pink");
+
+        $chart3Data = DB::table('idea')
+        ->rightJoin('users', 'users.id', '=', 'idea.user_id')
+        ->select(DB::raw('count(distinct(idea.user_id)) as number_of_user, users.department'))
+        ->groupBy('users.department')
+        ->get()
+        ->toArray();
+        $chart3XValues = array_column($chart3Data,'department');
+        $chart3YValues = array_column($chart3Data,'number_of_user');
 
         $chart2xTitle = array("Title 1", "Title 2", "Title 3", "Title 4", "Title 5","Title 6","Title 7");
         $chart2yTitle = array(65, 58, 80, 83, 55,53,40);
-        $chart2barColors = array("red", "green","blue","orange","brown","purple","yellow");
+        $chart2barColors = array("red", "green","blue","orange","brown","purple","yellow","black","pink");
         
         $data = ['LoggedUserInfo'=>User::where('id','=', session('LoggedUser'))->first()];
 
-        return view('dashboard', compact('titles','departments','ideasWithoutComment','chart1XValues','chart1yValues','chart1Colors','chart2xTitle','chart2yTitle','chart2barColors','anonymous'),  $data);
+        $ideasWithoutComment = DB::table('idea')->select('idea.*')->whereNotIn('id', DB::table('comments')->select('comments.idea_id'))->paginate(5, ['*'], 'ideas_without_comment');
+        return view('dashboard', compact('titles','departments','chart1XValues','chart1yValues','chart1Colors','chart2xTitle','chart2yTitle','chart2barColors','anonymousIdea','anonymousComment','contribution_of_title','chart3XValues','chart3YValues','ideasWithoutComment'),  $data);
     }
 
 
---
+++ discussion/resources/views/terms_idea.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="{{ URL::asset('css/terms.css') }}" rel="stylesheet">
+
+  <!-- All format -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  
+  <!-- Sidebar slide -->
+  <link href='https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css' rel='stylesheet'>
+  
+  <!-- Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+  <!-- Bootstrap 5 -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+        <script charset="utf8" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+</head>
+<body class="termsBody">
+  <div class="termsTitle">
+    <h1>Terms & Condition.</h1><br>
+    <p>Please check the terms and condition before submitting new idea.</p>
+      <div class="termsContent">When you agree to terms and conditions, you are basically agreeing to all sorts of things. In order to know exactly what you have agreed to, you have to read and make sure you understand everything in the terms and conditions. Most people are usually surprised when they find out what they have agreed to.
+        <br><br>When you agree to terms and conditions, you are basically agreeing to all sorts of things. In order to know exactly what you have agreed to, you have to read and make sure you understand everything in the terms and conditions. Most people are usually surprised when they find out what they have agreed to.
+      </div>
+  </div>
+</body>
+<script>
+</script>
+</html>
