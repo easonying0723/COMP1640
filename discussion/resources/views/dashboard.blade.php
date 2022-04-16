d96426f53885b149dbaf32b309c5e844d03b1f41
callmecam26 <100278322+callmecam26@users.noreply.github.com>
2022-04-16 22:17:40 +0800

    bug fixed

--- discussion/resources/views/dashboard.blade.php
+++ discussion/resources/views/dashboard.blade.php
@@ -321,6 +321,7 @@ table.ideas th {
     <th>User Name</th>
   </tr>
   @foreach($anonymousComment as $idea)
+  @if($idea->idea)
   <tr>
     <td>{{$idea->id}}</td>
     <td>{{$idea->comment}}</td>
@@ -328,6 +329,7 @@ table.ideas th {
     <td>{{$idea->user->id}}</td>
     <td>{{$idea->user->name}}</td>
   </tr>
+  @endif
   @endforeach
 
 </table>
