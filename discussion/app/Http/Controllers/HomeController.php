7e966001b26f244e77df1b88d985963823e62c7c
easonying0723 <ey73210723@gmail.com>
2022-04-17 17:30:01 +0800

    ya

--- discussion/app/Http/Controllers/HomeController.php
+++ discussion/app/Http/Controllers/HomeController.php
@@ -339,21 +339,6 @@ class HomeController extends Controller
 
 
     /////////////////////CATEGORY//////////////////////
-
-    public function Categoryindex(Request $request)
-    {
-       /*$search = $request['search'] ?? "";
-        if($search !="")
-         {
-             //where clause
-             $data = Cactegory::where('cate_name','LIKE', "%$search%")->get();
-        }else{
-             $data = Cactegory::all();
-         }*/
-         
-        
-       return view('homepage')->with('data',$data);
-    }
   
     public function category_store(Request $request)
     {
@@ -389,13 +374,6 @@ class HomeController extends Controller
 
     /////////////////////TITLE//////////////////////
 
-    public function titleIndex(Request $request)
-    {
-        $titleC = Title::with('title_name')->whereNull('id')->get;
-        
-        return view('/homepage',['titleC',$titleC]);
-
-    }
     public function title_store(Request $request)
     {
         $titleC = new Title;
